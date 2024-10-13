bb89818c7bc3d89f47e558646adac714cc017c59
AdamKurnia313 <adamkurnia313@gmail.com>
2024-10-13 22:02:00 +0700

    first commit

---
+++ lib/data/data_display.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'data_model.dart';
+
+class ApiService {
+  Future<Welcome?> fetchArticles() async {
+    final response = await http.get(
+        Uri.parse('https://my-json-server.typicode.com/Fallid/codelab-api/db'));
+
+    if (response.statusCode == 200) {
+      return Welcome.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load articles');
+    }
+  }
+}
